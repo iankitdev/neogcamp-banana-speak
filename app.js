21867685eaa063fa41d647af3255c66e654fed6a
Ankit Sharma <iankitdev@gmail.com>
2022-12-21 00:19:07 +0530

    feat: input output processing

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function clickHandler(){
-    outputDiv.innerText= "uyguyguiygguyi" + txtInput.value;
-};
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
 
-btnTranslate.addEventListener("click", clickHandler)
+function errorHandler(error) {
+  console.log("error occured ", error);
+  alert("something wrong with server! try again after some time");
+}
+
+function clickHandler() {
+  
+  var inputText = txtInput.value; // taking input
+
+  // calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText; // output
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
